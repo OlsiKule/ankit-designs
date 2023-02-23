489ce94f90e51474f2c31fd8e4d2a34dabf8eec3
olsikule <olsi.kule@gmail.com>
2023-02-23 12:46:45 -0500

    hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,46 +1,22 @@
-// JavaScript code to toggle navbar visibility
-// let prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   let currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.querySelector('.navbar').classList.add('upScroll');
-//   } else {
-//     document.querySelector('.navbar').classList.remove('upScroll');
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
-
-
-
-
-// Get the navbar element
+// Hide navbar on scroll up
 const navbar = document.querySelector('.navbar');
-
-// Initialize the previous scroll position to the current scroll position
 let prevScrollPos = window.pageYOffset;
 
-// Add a scroll event listener to the window object
 window.addEventListener('scroll', () => {
-  // Get the current scroll position
   const currentScrollPos = window.pageYOffset;
-
-  // Check if the user is scrolling up
   if (prevScrollPos > currentScrollPos) {
-    // Add the "hide" class to the navbar element
     navbar.classList.remove('hide');
   } else {
-    // Remove the "hide" class from the navbar element
     navbar.classList.add('hide');
   }
 
-  // Update the previous scroll position to the current scroll position
   prevScrollPos = currentScrollPos;
 });
 
 
 
 
-// JavaScript code to toggle navbar sticky class
+// Toggle navbar to sticky 
 window.onscroll = function() {
     let navbar = document.querySelector('.navbar');
     let roadmap = document.querySelector('.roadmap');
@@ -54,58 +30,32 @@ window.onscroll = function() {
   }
 
 
-// ChatGPT suggestion
-
-// const navbar = document.querySelector('.navbar');
-// const roadmap = document.querySelector('.roadmap');
-// const contentTop = roadmap.offsetTop + 50;
-
-// window.addEventListener('scroll', () => {
-//   const currentScrollPos = window.pageYOffset;
-
-//   if (prevScrollPos > currentScrollPos) {
-//     navbar.classList.remove('hide');
-//   } else {
-//     navbar.classList.add('hide');
-//   }
-
-//   if (window.pageYOffset > contentTop) {
-//     navbar.classList.add('active');
-//   } else {
-//     navbar.classList.remove('active');
-//   }
-
-//   prevScrollPos = currentScrollPos;
-// });
-
-
-
-// hamburger menu
+// Hamburger menu
 const btnHamburger = document.querySelector("#btnHamburger");
 const body = document.querySelector("body");
 const header = document.querySelector(".header");
 const overlay = document.querySelector(".overlay");
 const fadeElems = document.querySelectorAll(".has-fade")
 
-// btnHamburger.addEventListener("click", function(){
-    // console.log("click hamburger"));
+btnHamburger.addEventListener("click", function(){
+    // console.log("click hamburger");
 
 
-    // if(header.classList.contains("open")){ //Close hamburger menu 
-    //     body.classList.remove("noscroll")
-    //     header.classList.remove("open");
-    //     fadeElems.forEach(function(element){
-    //         element.classList.remove("fade-in")
-    //         element.classList.add("fade-out")
-    //     })
+    if(header.classList.contains("open")){ //Close hamburger menu 
+        body.classList.remove("noscroll")
+        header.classList.remove("open");
+        fadeElems.forEach(function(element){
+            element.classList.remove("fade-in")
+            element.classList.add("fade-out")
+        })
         
 
-    // }else{ //Open hamburger menu 
-    //     body.classList.add("noscroll")
-    //     header.classList.add("open");
-    //     fadeElems.forEach(function(element){
-    //         element.classList.remove("fade-out")
-    //          element.classList.add("fade-in")
-    //     })
-    // }
-// })  
+    }else{ //Open hamburger menu 
+        body.classList.add("noscroll")
+        header.classList.add("open");
+        fadeElems.forEach(function(element){
+            element.classList.remove("fade-out")
+             element.classList.add("fade-in")
+        })
+    }
+}) ;
