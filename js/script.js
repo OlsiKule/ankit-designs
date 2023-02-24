3f189501c874b64f56bce1c3fe8439b8fe9b6ec7
olsikule <olsi.kule@gmail.com>
2023-02-24 00:44:12 -0500

    completed hamburger menu

--- js/script.js
+++ js/script.js
@@ -31,31 +31,39 @@ window.onscroll = function() {
 
 
 // Hamburger menu
-const btnHamburger = document.querySelector("#btnHamburger");
-const body = document.querySelector("body");
-const header = document.querySelector(".header");
-const overlay = document.querySelector(".overlay");
-const fadeElems = document.querySelectorAll(".has-fade")
+// const btnHamburger = document.querySelector("#btnHamburger");
+// const body = document.querySelector("body");
+// const header = document.querySelector(".header");
+// const overlay = document.querySelector(".overlay");
+// const fadeElems = document.querySelectorAll(".has-fade")
 
-btnHamburger.addEventListener("click", function(){
-    // console.log("click hamburger");
+// btnHamburger.addEventListener("click", function(){
+//     // console.log("click hamburger");
 
 
-    if(header.classList.contains("open")){ //Close hamburger menu 
-        body.classList.remove("noscroll")
-        header.classList.remove("open");
-        fadeElems.forEach(function(element){
-            element.classList.remove("fade-in")
-            element.classList.add("fade-out")
-        })
+//     if(header.classList.contains("open")){ //Close hamburger menu 
+//         body.classList.remove("noscroll")
+//         header.classList.remove("open");
+//         fadeElems.forEach(function(element){
+//             element.classList.remove("fade-in")
+//             element.classList.add("fade-out")
+//         })
         
 
-    }else{ //Open hamburger menu 
-        body.classList.add("noscroll")
-        header.classList.add("open");
-        fadeElems.forEach(function(element){
-            element.classList.remove("fade-out")
-             element.classList.add("fade-in")
-        })
-    }
-}) ;
+//     }else{ //Open hamburger menu 
+//         body.classList.add("noscroll")
+//         header.classList.add("open");
+//         fadeElems.forEach(function(element){
+//             element.classList.remove("fade-out")
+//              element.classList.add("fade-in")
+//         })
+//     }
+// }) ;
+
+const hamburger = document.querySelector(".header__toggle");
+const menu = document.querySelector(".header__menu");
+
+hamburger.addEventListener( "click", () => {
+    hamburger.classList.toggle("onOff")
+    menu.classList.toggle("has-fade")
+})
