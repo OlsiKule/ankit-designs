3908c9cf5e0b4f648207880d4c416889fecc3907
olsikule <olsi.kule@gmail.com>
2023-04-25 13:16:11 -0400

    removed previous commented code

--- js/script.js
+++ js/script.js
@@ -4,14 +4,15 @@ let prevScrollPos = window.pageYOffset;
 
 window.addEventListener('scroll', () => {
   const currentScrollPos = window.pageYOffset;
+  // looks at the Y coordinates on page and compares current page positioning with previous one to show or hide navbar
   if (prevScrollPos > currentScrollPos) {
     navbar.classList.remove('hide');
   } else {
     navbar.classList.add('hide');
   }
-
   prevScrollPos = currentScrollPos;
 });
+
 
 // Toggle navbar to sticky
 window.onscroll = function () {
@@ -27,33 +28,6 @@ window.onscroll = function () {
 };
 
 // Hamburger menu
-// const btnHamburger = document.querySelector("#btnHamburger");
-// const body = document.querySelector("body");
-// const header = document.querySelector(".header");
-// const overlay = document.querySelector(".overlay");
-// const fadeElems = document.querySelectorAll(".has-fade")
-
-// btnHamburger.addEventListener("click", function(){
-//     // console.log("click hamburger");
-
-//     if(header.classList.contains("open")){ //Close hamburger menu
-//         body.classList.remove("noscroll")
-//         header.classList.remove("open");
-//         fadeElems.forEach(function(element){
-//             element.classList.remove("fade-in")
-//             element.classList.add("fade-out")
-//         })
-
-//     }else{ //Open hamburger menu
-//         body.classList.add("noscroll")
-//         header.classList.add("open");
-//         fadeElems.forEach(function(element){
-//             element.classList.remove("fade-out")
-//              element.classList.add("fade-in")
-//         })
-//     }
-// }) ;
-
 const hamburger = document.querySelector('.header__toggle');
 const menu = document.querySelector('.header__menu');
 const closeMenu = document.querySelector('.closeMenu');
